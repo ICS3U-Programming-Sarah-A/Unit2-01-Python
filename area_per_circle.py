18115e4dd84e96219a24f13e0509dfe223750ac0
Ubuntu <ubuntu@ip-172-31-55-166.ec2.internal>
2022-02-27 19:36:55 +0000

    Made area_per_circle in Python.

---
+++ area_per_circle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+# Created By: Sarah
+# Date: Feb. 27, 2022
+# This program calculates and displays the area and perimeter of a
+# circle with a radius of 12 mm.
+import math
+
+
+def main():
+    print("For a circle that has a radius")
+    print("of 12 mm:")
+    print("")
+    print("Area = {} mm^2". format(math.pi*12**2))
+    print("Perimeter = {} mm". format(math.pi*2*12))
+
+
+if __name__ == "__main__":
+    main()
